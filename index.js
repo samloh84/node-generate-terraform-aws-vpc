f980d260c8cc6e737e408e6d34eca89647b4654f
Samuel Loh Yin Kang <samuel.loh.yk@gmail.com>
2019-04-24 00:44:31 +0800

    Revamped rendering function to accumulate rendered templates Revamped config parser to normalize input config file Added command line parsing to choose input config file

--- index.js
+++ index.js
@@ -1,5 +1,38 @@
+#!/usr/bin/env node
+
 const parse = require('./lib/parse');
 const render = require('./lib/render');
-let config = parse.parse('config.yaml');
+const commander = require('commander');
+const nodePath = require('path');
+const _ = require('lodash');
+const util =require('util');
+
+
+let config_path = nodePath.resolve(process.cwd(), 'config.yaml');
+commander
+    .version('0.1.1')
+    .arguments('[config_path]')
+    .action(render_output);
+
+commander.parse(process.argv);
+
+function render_output(arg_config_path) {
+    if (_.isNil(arg_config_path)) {
+        console.log(`Config path not specified, defaulting to : ${config_path}`);
+    } else {
+        config_path = nodePath.resolve(process.cwd(), arg_config_path);
+        console.log(`Config path: ${config_path}`);
+    }
+}
+
+
+
+function debug_log(obj) {
+    console.error(JSON.stringify(obj, null, 4))
+}
+
+let config = parse.parse(config_path);
+debug_log(config);
 
 render.render(config);
+
